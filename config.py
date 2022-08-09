b36f01a95a8284de1f0ae75c5cfc9a91ec69b7c8
Kishore Kumar Ganta <76893429+kishorekumarganta@users.noreply.github.com>
2022-08-10 01:25:28 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+
+#credentials
+user    = ' '
+pwd     = ' '
+factor2 = ' '
+vc      = ' '
+app_key =' '
+imei    = ' '
+
+
+
+userid    = ' '
+password     = ' '
+twoFA = ''
+vendor_code      = ''
+api_secret =''
+
+
+
+
